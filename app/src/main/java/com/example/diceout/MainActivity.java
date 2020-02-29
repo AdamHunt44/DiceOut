a0c6fc8a002ba0cf56ab351fe9ff0dad4acea348
Adam Hunt <adamhunt@pop-os.localdomain>
2020-02-29 12:02:47 +0000

    Updated UI to use the floating action button instead of a regular button

--- app/src/main/java/com/example/diceout/MainActivity.java
+++ app/src/main/java/com/example/diceout/MainActivity.java
@@ -3,13 +3,11 @@ package com.example.diceout;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -34,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
 
     // UI components
     TextView rollResult;
-    Button rollButton;
 
     // Field to hold the score text
     TextView scoreText;
@@ -50,8 +47,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                rollDice(view);
             }
         });
 
@@ -59,7 +55,6 @@ public class MainActivity extends AppCompatActivity {
         score = 0;
 
         rollResult = findViewById(R.id.rollResult);
-        rollButton = findViewById(R.id.rollButton);
         scoreText = findViewById(R.id.scoreText);
         rand = new Random();
 
